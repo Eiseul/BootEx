175989571e9eaebe40e3297e264e2f635d618970
SJF02 <Foaming21@gmail.com>
2025-06-04 17:36:22 +0900

    SpringBoot_web

--- boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
@@ -2,13 +2,17 @@ package org.suhodo.boot01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suhodo.boot01.dto.BoardDTO;
 import org.suhodo.boot01.dto.PageRequestDTO;
 import org.suhodo.boot01.dto.PageResponseDTO;
 import org.suhodo.boot01.service.BoardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -28,5 +32,41 @@ public class BoardController {
         log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    // register.html을 보여주는 역할
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    // register.html에서 보내온 데이터를 처리하는 역할
+    /*
+     * boardDTO에 브라우저가 보내준 변수값이 저장되는 지 검증 @Valid
+     * 검증과정에서 이상있으면 bindingResult에 에러가 발생
+     */
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes){
+    
+        log.info("board POST register...........");
+
+        // 데이터를 넣는 과정에서 에러가 발생했다면
+        if(bindingResult.hasErrors()){
+            log.info("has errors...........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            // 브라우저한테 /board/register로 즉시 이동해 명령
+            // 위의 errors값을 가지고 이동, 한번 보이면 이후에는 사라짐
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
 }
