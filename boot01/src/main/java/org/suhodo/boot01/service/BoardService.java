88611c472087abb91201e48c7030427282881c93
SJF02 <Foaming21@gmail.com>
2025-06-04 15:09:05 +0900

    SpringBoot

---
+++ boot01/src/main/java/org/suhodo/boot01/service/BoardService.java
@@ -0,0 +1,14 @@
+package org.suhodo.boot01.service;
+
+import org.suhodo.boot01.dto.BoardDTO;
+import org.suhodo.boot01.dto.PageRequestDTO;
+import org.suhodo.boot01.dto.PageResponseDTO;
+
+public interface BoardService {
+
+    Long register(BoardDTO boardDTO);
+    BoardDTO readOne(Long bno);
+    void modify(BoardDTO boardDTO);
+    void remove(Long bno);
+    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
+}
